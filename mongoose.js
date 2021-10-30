c0bb1ba3f1fb060d217578c275ce613d647dac75
V1ZEI-CN <vijaychowdary3699@gmail.com>
2021-10-30 09:38:50 +0530

    schema + insertData + close connection

--- mongoose.js
+++ mongoose.js
@@ -6,9 +6,29 @@ async function main(){
   try{
     await mongoose.connect("mongodb://127.0.0.1:27017/mongoose_db")
     console.log("Connection Success");
-  }catch(err){
-    console.log(err);
-  }finally{
-    mongoose.connection.close()
+
+    const blogSchema = new mongoose.Schema({
+      title: String,
+      author: String,
+      content: String,
+      comments: [{body: String, date: Date}],
+      hidden: Boolean,
+      metaData: {
+        votes: Number
+      }
+    })
+
+    const blogPost = mongoose.model("Blog", blogSchema)
+
+    const newPost = new blogPost({
+      title: 'This is trail inside main',
+      author: "V1-ZEI",
+      content: "make the save function as await as that returns promise",
+    })
+
+    await newPost.save();
+
   }
+  catch(err){ console.log(err); }
+  finally{ mongoose.connection.close(); }
 }
