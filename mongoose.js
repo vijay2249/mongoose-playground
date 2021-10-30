c4f348d5d08c8f7327cfdd3c735036e303fbe9b6
V1ZEI-CN <vijaychowdary3699@gmail.com>
2021-10-30 12:08:56 +0530

    filter and find in mongoose

--- mongoose.js
+++ mongoose.js
@@ -7,18 +7,18 @@ async function main(){
     await mongoose.connect("mongodb://127.0.0.1:27017/mongoose_db")
     console.log("Connection Success");
 
-    // const blogSchema = new mongoose.Schema({
-    //   title: String,
-    //   author: String,
-    //   content: String,
-    //   comments: [{body: String, date: Date}],
-    //   hidden: Boolean,
-    //   metaData: {
-    //     votes: Number
-    //   }
-    // })
+    const blogSchema = new mongoose.Schema({
+      title: String,
+      author: String,
+      content: String,
+      comments: [{body: String, date: Date}],
+      hidden: Boolean,
+      metaData: {
+        votes: Number
+      }
+    })
 
-    // const blogPost = mongoose.model("Blog", blogSchema)
+    const blogPost = mongoose.model("Blog", blogSchema)
 
     // const newPost = new blogPost({
     //   title: 'This is trail inside main',
@@ -27,12 +27,21 @@ async function main(){
     // })
     // await newPost.save();
 
-    const personSchema = new mongoose.Schema({
-      name: String,
-      age: Number
+    await blogPost.find({author: 'V1-ZEI'}, function(err, filteredOutput){
+      if(err){
+        console.log(err);
+      }else{
+        console.log(filteredOutput);
+      }
+      mongoose.connection.close()
     })
 
-    const person = mongoose.model("Person", personSchema)
+    // const personSchema = new mongoose.Schema({
+    //   name: String,
+    //   age: Number
+    // })
+
+    // const person = mongoose.model("Person", personSchema)
 
     // const newPerson = new person({
     //   name: 'Idiot',
@@ -42,28 +51,29 @@ async function main(){
     // const result = await newPerson.save()
     // console.log(`New person added with id: ${result._id}`);
 
-    const vijay = new person({
-      name: 'VIJAY',
-      age: 20
-    })
-    const ajay = new person({
-      name: 'AJAY',
-      age: 22
-    })
-    const sai = new person({
-      name: 'SAI',
-      age: 25
-    })
+    // const vijay = new person({
+    //   name: 'VIJAY',
+    //   age: 20
+    // })
+    // const ajay = new person({
+    //   name: 'AJAY',
+    //   age: 22
+    // })
+    // const sai = new person({
+    //   name: 'SAI',
+    //   age: 25
+    // })
 
-    await person.insertMany([vijay, ajay, sai], function(err){
-      if(err){
-        console.log(err);
-      }else{
-        console.log("Insertion success");
-      }
-    })
+    // await person.insertMany([vijay, ajay, sai], function(err){
+    //   if(err){
+    //     console.log(err);
+    //   }else{
+    //     console.log("Insertion success");
+    //   }
+    // })
 
   }
   catch(err){ console.log(err); }
-  finally{ mongoose.connection.close(); }
 }
+
+// mongoose.connection.close();
