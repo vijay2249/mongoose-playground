a4c66a5ee4b09bb268c5902a5a1a5fa6e4c81e82
V1ZEI-CN <vijaychowdary3699@gmail.com>
2021-10-30 09:54:38 +0530

    multiple schemas can be inside one database

--- mongoose.js
+++ mongoose.js
@@ -7,26 +7,40 @@ async function main(){
     await mongoose.connect("mongodb://127.0.0.1:27017/mongoose_db")
     console.log("Connection Success");
 
-    const blogSchema = new mongoose.Schema({
-      title: String,
-      author: String,
-      content: String,
-      comments: [{body: String, date: Date}],
-      hidden: Boolean,
-      metaData: {
-        votes: Number
-      }
+    // const blogSchema = new mongoose.Schema({
+    //   title: String,
+    //   author: String,
+    //   content: String,
+    //   comments: [{body: String, date: Date}],
+    //   hidden: Boolean,
+    //   metaData: {
+    //     votes: Number
+    //   }
+    // })
+
+    // const blogPost = mongoose.model("Blog", blogSchema)
+
+    // const newPost = new blogPost({
+    //   title: 'This is trail inside main',
+    //   author: "V1-ZEI",
+    //   content: "make the save function as await as that returns promise"
+    // })
+    // await newPost.save();
+
+    const personSchema = new mongoose.Schema({
+      name: String,
+      age: Number
     })
 
-    const blogPost = mongoose.model("Blog", blogSchema)
+    const person = mongoose.model("Person", personSchema)
 
-    const newPost = new blogPost({
-      title: 'This is trail inside main',
-      author: "V1-ZEI",
-      content: "make the save function as await as that returns promise",
+    const newPerson = new person({
+      name: 'Idiot',
+      age: 25
     })
 
-    await newPost.save();
+    const result = await newPerson.save()
+    console.log(`New person added with id: ${result._id}`);
 
   }
   catch(err){ console.log(err); }
